c9d9b308a6fbaf1ee216548d83a579af3d3d2d9f
Marlon-44 <marlondavidag44@outlook.com>
2025-06-19 23:56:40 -0500

    arreglo e reviews

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import AppRoutes from './routes.jsx'
 import PlanesProvider from './features/planes/PlanesProvider.jsx'
 import { ServiceProvider } from './Context/servicesContext.jsx'
-import { ReviewsProvider } from './features/Reviews/ReviewsProvider.jsx'
+import { ReviewsProvider } from './features/reviews/ReviewsProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
